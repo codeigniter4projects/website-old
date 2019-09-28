6286ef016782f6c74bfbb8fab77350b02730da1f
thomaswong <thomasfwong@hotmail.com>
2019-09-28 16:31:08 -0700

    Ca-EN translation

---
+++ app/Language/en-ca/Site.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Application-wide messages for the CodeIgniter website
+ * The text blocks use markdown format.
+ */
+$footerLine = <<<'EOS'
+CodeIgniter was created by [EllisLab](http://www.ellislab.com/), 
+fostered by [BCIT](http://www.bcit.ca/cas/computing/), 
+and will soon be a project of the CodeIgniter Foundation.
+[<span class="glyphicon glyphicon-envelope"></span>](mailto:admin@codeigniter.com)
+EOS;
+
+return [
+	'topNav1'		 => '<span class="glyphicon glyphicon-home"></span>',
+	'topNav2'		 => 'Download',
+	'topNav3'		 => 'Documentation',
+	'topNav4'		 => 'Community',
+	'topNav5'		 => 'Contribute',
+	'bottomNav1'	 => 'Policies',
+	'bottomNav2'	 => 'The Fine Print',
+	'bottomNav3'	 => 'About',
+	'footerLine'	 => $footerLine,
+];
