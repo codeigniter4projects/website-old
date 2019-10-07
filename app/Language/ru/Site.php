91265f9a9974dc804ab27cc6d004f6f1a7d6116c
DigitalWolf98 <digital.wolf98@gmail.com>
2019-10-07 23:09:35 +0600

    Adding Russian translation

---
+++ app/Language/ru/Site.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Application-wide messages for the CodeIgniter website
+ * The text blocks use markdown format.
+ */
+$footerLine = <<<'EOS'
+CodeIgniter был создан [EllisLab](http://www.ellislab.com/), 
+при поддержке [BCIT](http://www.bcit.ca/cas/computing/)
+и скоро станет проектом Фонда CodeIgniter.
+[<span class="glyphicon glyphicon-envelope"></span>](mailto:admin@codeigniter.com)
+EOS;
+
+return [
+	'topNav1'		 => '<span class="glyphicon glyphicon-home"></span>',
+	'topNav2'		 => 'Скачать',
+	'topNav3'		 => 'Документация',
+	'topNav4'		 => 'Сообщество',
+	'topNav5'		 => 'Содейсвие',
+	'bottomNav1'	 => 'Политика',
+	'bottomNav2'	 => 'Бренд',
+	'bottomNav3'	 => 'О нас',
+	'footerLine'	 => $footerLine,
+];
