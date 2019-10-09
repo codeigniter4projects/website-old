66dd8f7bc6fb0c3231df1629a84c1f6fe38b44cc
Cathy Chan <chan.cathy@hotmail.ca>
2019-10-09 12:39:07 -0700

    french site translation

---
+++ app/Language/fr/Site.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Application-wide messages for the CodeIgniter website
+ * The text blocks use markdown format.
+ */
+$footerLine = <<<'EOS'
+CodeIgniter a été créé par [EllisLab](http://www.ellislab.com/), 
+soutenu par le [BCIT](http://www.bcit.ca/cas/computing/), 
+et sera bientôt un projet de la Fondation CodeIgniter.
+[<span class="glyphicon glyphicon-envelope"></span>](mailto:admin@codeigniter.com)
+EOS;
+
+return [
+	'topNav1'		 => '<span class="glyphicon glyphicon-home"></span>',
+	'topNav2'		 => 'Télécharger',
+	'topNav3'		 => 'Documentation',
+	'topNav4'		 => 'Communauté',
+	'topNav5'		 => 'Contribuer',
+	'bottomNav1'	 => 'Stratégies',
+	'bottomNav2'	 => 'Les petits caractères',
+	'bottomNav3'	 => 'À propos',
+	'footerLine'	 => $footerLine,
+];
