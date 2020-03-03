50609ef9eabc80ab3420de7d93666030f0f86500
Lonnie Ezell <lonnieje@gmail.com>
2020-03-02 23:54:03 -0600

    Refactor docs page

--- app/Controllers/Docs.php
+++ app/Controllers/Docs.php
@@ -5,7 +5,7 @@ namespace App\Controllers;
  * controllers/docs.php
  *
  * Render the current version of the user guide.
- * 
+ *
  * CodeIgniter website, template driven
  *
  */
@@ -18,30 +18,9 @@ class Docs extends BaseController
 
 	function index()
 	{
-		$this->data['title'] = lang('Docs.title');
-		$this->data['pagebody'] = 'docs';
+		$this->data['title'] = 'CodeIgniter Documentation';
 
-		// localized page pieces
-		$this->localize('Docs', 'heading');
-		$this->localize('Docs', 'subheading');
-		$this->localize('Docs', 'onlineButton');
-		$this->localize('Docs', 'offlineButton');
-		$this->localize('Docs', 'apidocsButton');
-		$this->localize('Docs', 'epubButton');
-
-		// CI4 block, with nested substitution
-		$this->localize('Download', 'ci4Title');
-		$original = $this->parsedown->text(lang('Docs.ci4Text'));
-		$this->data['ci4Text'] = $this->parser->setData($this->data, 'raw')
-				->renderString($original);
-
-		// CI3 block, with nested substitution
-		$this->localize('Download', 'ci3Title');
-		$original = $this->parsedown->text(lang('Docs.ci3Text'));
-		$this->data['ci3Text'] = $this->parser->setData($this->data, 'raw')
-				->renderString($original);
-
-		$this->render();
+		$this->render('docs', $this->data);
 	}
 
 }
