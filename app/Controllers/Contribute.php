67ca1bfd773cf8f6a1a7c798af996ffb532d9a34
Lonnie Ezell <lonnieje@gmail.com>
2020-03-04 23:57:47 -0600

    Cleanup Contribute controller by moving Hero stuff to the GithubHelper class.

--- app/Controllers/Contribute.php
+++ app/Controllers/Contribute.php
@@ -12,9 +12,6 @@ namespace App\Controllers;
  */
 class Contribute extends BaseController
 {
-
-	private $astar = '★';
-
 	//-------------------------------------------------------------
 	//  The normal pages
 	//-------------------------------------------------------------
@@ -24,62 +21,8 @@ class Contribute extends BaseController
      */
 	public function index()
 	{
+	    $this->data = $this->gitter->fillHeroes($this->data);
 		$this->data['title'] = 'Contribute to CodeIgniter';
-		$gitter = new \App\Libraries\GithubAPI();
-
-		// get the framework heros
-		if ( ! $info = cache('fw_heros'))
-		{
-			$info = $gitter->getContributors('bcit-ci', 'CodeIgniter');
-			$ttl = 60 * 60 * 4; // time to live s/b 4 hours
-			cache()->save('fw_heros', $info, $ttl);
-		}
-		$this->data['fw_heros'] = $this->hitparade($info);
-
-		// get the translation heros
-		if ( ! $info = cache('trans_heros'))
-		{
-			$info = $gitter->getContributors('bcit-ci', 'codeigniter3-translations');
-			$ttl = 60 * 60 * 4; // time to live s/b 4 hours
-			cache()->save('trans_heros', $info, $ttl);
-		}
-		$this->data['trans_heros'] = $this->hitparade($info);
-
-		// get the CodeIgniter4 heros
-		if ( ! $info = cache('ci4_heros'))
-		{
-			$info = $gitter->getContributors('codeigniter4', 'CodeIgniter4');
-			$ttl = 60 * 60 * 4; // time to live s/b 4 hours
-			cache()->save('ci4_heros', $info, $ttl);
-		}
-		$this->data['ci4_heros'] = $this->hitparade($info);
-
-		// get the translation heros
-		if ( ! $info = cache('trans4_heros'))
-		{
-			$info = $gitter->getContributors('codeigniter4', 'translations');
-			$ttl = 60 * 60 * 4; // time to live s/b 4 hours
-			cache()->save('trans4_heros', $info, $ttl);
-		}
-		$this->data['trans4_heros'] = $this->hitparade($info);
-
-		// get the website heros
-		if ( ! $info = cache('web_heros'))
-		{
-			$info = $gitter->getContributors('bcit-ci', 'codeigniter-website');
-			$ttl = 60 * 60 * 4; // time to live s/b 4 hours
-			cache()->save('web_heros', $info, $ttl);
-		}
-		$this->data['web_heros'] = $this->hitparade($info);
-
-		// and the new website heros
-		if ( ! $info = cache('new_web_heros'))
-		{
-			$info = $gitter->getContributors('codeigniter4projects', 'website2');
-			$ttl = 60 * 60 * 4; // time to live s/b 4 hours
-			cache()->save('new_web_heros', $info, $ttl);
-		}
-		$this->data['new_web_heros'] = $this->hitparade($info);
 
 		$this->render('contribute');
 	}
@@ -93,50 +36,4 @@ class Contribute extends BaseController
 
 		$this->render('responsible');
 	}
-
-    /**
-     * build the hit parade for this group of contributors
-     *
-     * @param $info
-     *
-     * @return string
-     */
-	protected function hitparade($info)
-	{
-		$heros = [];
-		if ( ! empty($info))
-		{
-			foreach ($info as $val)
-			{
-				$heros[] = [
-					'avatar' => $val['avatar_url'],
-					'name'	 => $val['login'],
-					'url'	 => $val['html_url'],
-					'stars'	 => $this->stars($val['contributions'])
-				];
-			}
-			return view('theme/_heros', ['heros' => $heros]);
-		}
-		else
-			return '';
-	}
-
-    /**
-     * determine how many stars a contributor earns
-     *
-     * @param $contributions
-     *
-     * @return string
-     */
-	protected function stars($contributions)
-	{
-		$result = $this->astar;
-		while ($contributions > 9)
-		{
-			$result .= $this->astar;
-			$contributions /= 10;
-		}
-		return $result;
-	}
-
 }
--- app/Libraries/GitHubHelper.php
+++ app/Libraries/GitHubHelper.php
@@ -7,6 +7,8 @@ class GitHubHelper
      */
     protected $api;
 
+    protected $astar = '★';
+
     public function __construct()
     {
         $this->api = new GithubAPI();
@@ -45,5 +47,113 @@ class GitHubHelper
         $data['v3trans'] = $info5['tag_name'];
 
         return $data;
+    }
+
+    /**
+     * Grab the "hero" data for our contribute page from Github,
+     * caching to speed things up.
+     *
+     * @param array $data
+     *
+     * @return array
+     */
+    public function fillHeroes(array $data): array
+    {
+        $ttl = config('App')->gitHubExpires;
+
+        // get the framework heros
+        if ( ! $info = cache('fw_heros'))
+        {
+            $info = $this->api->getContributors('bcit-ci', 'CodeIgniter');
+            cache()->save('fw_heros', $info, $ttl);
+        }
+        $data['fw_heros'] = $this->hitparade($info);
+
+        // get the translation heros
+        if ( ! $info = cache('trans_heros'))
+        {
+            $info = $this->api->getContributors('bcit-ci', 'codeigniter3-translations');
+            cache()->save('trans_heros', $info, $ttl);
+        }
+        $data['trans_heros'] = $this->hitparade($info);
+
+        // get the CodeIgniter4 heros
+        if ( ! $info = cache('ci4_heros'))
+        {
+            $info = $this->api->getContributors('codeigniter4', 'CodeIgniter4');
+            cache()->save('ci4_heros', $info, $ttl);
+        }
+        $data['ci4_heros'] = $this->hitparade($info);
+
+        // get the translation heros
+        if ( ! $info = cache('trans4_heros'))
+        {
+            $info = $this->api->getContributors('codeigniter4', 'translations');
+            cache()->save('trans4_heros', $info, $ttl);
+        }
+        $data['trans4_heros'] = $this->hitparade($info);
+
+        // get the website heros
+        if ( ! $info = cache('web_heros'))
+        {
+            $info = $this->api->getContributors('bcit-ci', 'codeigniter-website');
+            cache()->save('web_heros', $info, $ttl);
+        }
+        $data['web_heros'] = $this->hitparade($info);
+
+        // and the new website heros
+        if ( ! $info = cache('new_web_heros'))
+        {
+            $info = $this->api->getContributors('codeigniter4projects', 'website2');
+            cache()->save('new_web_heros', $info, $ttl);
+        }
+        $data['new_web_heros'] = $this->hitparade($info);
+
+        return $data;
+    }
+
+    /**
+     * build the hit parade for this group of contributors
+     *
+     * @param $info
+     *
+     * @return string
+     */
+    protected function hitparade($info)
+    {
+        $heros = [];
+        if ( ! empty($info))
+        {
+            foreach ($info as $val)
+            {
+                $heros[] = [
+                    'avatar' => $val['avatar_url'],
+                    'name'	 => $val['login'],
+                    'url'	 => $val['html_url'],
+                    'stars'	 => $this->stars($val['contributions'])
+                ];
+            }
+            return view('theme/_heros', ['heros' => $heros]);
+        }
+        else
+            return '';
+    }
+
+    /**
+     * determine how many stars a contributor earns
+     *
+     * @param $contributions
+     *
+     * @return string
+     */
+    protected function stars($contributions)
+    {
+        $result = $this->astar;
+        while ($contributions > 9)
+        {
+            $result .= $this->astar;
+            $contributions /= 10;
+        }
+        return $result;
     }
 }
